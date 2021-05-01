650fc1e43e9e31d8212c32d5cde07eec69e712df
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2021-05-01 16:26:20 -0700

    SPC: Make line-length 90 characters for black.
    --HG--
    branch : 0.4.0

--- bin/mmf_setup_bash.py
+++ bin/mmf_setup_bash.py
@@ -10,8 +10,8 @@ import mmf_setup
 #    export variable=value
 #
 VARIABLES = [
-    ('MMF_SETUP', mmf_setup.MMF_SETUP, mmf_setup.MMF_SETUP),
-    ('HGRCPATH', ':'.join(['${HGRCPATH:-~/.hgrc}', mmf_setup.HGRC]), mmf_setup.HGRC),
+    ("MMF_SETUP", mmf_setup.MMF_SETUP, mmf_setup.MMF_SETUP),
+    ("HGRCPATH", ":".join(["${HGRCPATH:-~/.hgrc}", mmf_setup.HGRC]), mmf_setup.HGRC),
 ]
 
 
@@ -21,17 +21,25 @@ def run(debug=False):
         if not filename or exists(filename):
             env.append('export {var}="{value}"'.format(var=var, value=value))
         elif debug:
-            print("# processing {}={} failed:\n   no file '{}'"
-                  .format(var, value, filename))
+            print(
+                "# processing {}={} failed:\n   no file '{}'".format(
+                    var, value, filename
+                )
+            )
 
     print("\n".join(env))
 
 
 parser = OptionParser()
-parser.add_option("-d", "--debug",
-                  action="store_true", dest="debug", default=False,
-                  help="debug missing files")
+parser.add_option(
+    "-d",
+    "--debug",
+    action="store_true",
+    dest="debug",
+    default=False,
+    help="debug missing files",
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     (options, args) = parser.parse_args()
     run(options.debug)
